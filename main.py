9a487a6a192a97c46d126619ab3e94c7f2fb590c
henrique15775 <61325756+henrique15775@users.noreply.github.com>
2021-06-02 12:34:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,56 +12,56 @@ for i in range(20000):
 while True:
   os.system("clear")
   print('#1-BubbleSort\n#2-InsertionSort\n#3-ShellSort\n#4-MergeSort\n#5-QuickSort\n#6-Quit\n\n')
-  interacao = int(input("Digite a opção"))
+  opcao = int(input("Digite a opção -> "))
 
-  if (interacao == 1):
+  if (opcao == 1):
     lista_alt = lista.copy()
-    print(f"\n{lista}\n\n")
+    
     inicio = time.time()
     BubbleSort(lista_alt)
     fim = time.time()
     total = (fim - inicio)
     print(f"Tempo levado com o BubbleSort {total}s")
-    acessarMenu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
+    confirma_volta_menu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
 
-    if (acessarMenu == 1):
+    if (confirma_volta_menu == 1):
       pass
     else:
       quit()
   
-  elif (interacao == 2):
+  elif (opcao == 2):
     lista_alt = lista.copy()
-    print(f"\n{lista}\n")
+    
     inicio = time.time()
     InsertionSort(lista_alt)
     fim = time.time()
     total = (fim - inicio)
     print(f" Tempo levado com o InsertionSort {total}s")
-    acessarMenu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
+    confirma_volta_menu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
 
-    if (acessarMenu == 1):
+    if (confirma_volta_menu == 1):
       pass
     else:
       quit()
   
-  elif (interacao == 3):
+  elif (opcao == 3):
     lista_alt = lista.copy()
-    print(f"\n{lista}\n")
+    
     inicio = time.time()
     ShellSort(lista_alt)
     fim = time.time()
     total = (fim - inicio)
     print(f"Tempo levado com o ShellSort {total}s")
-    acessarMenu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
+    confirma_volta_menu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
 
-    if (acessarMenu == 1):
+    if (confirma_volta_menu == 1):
       pass
     else:
       quit()
   
-  elif (interacao == 4):
+  elif (opcao == 4):
     lista_alt = lista.copy()
-    print(f"\n{lista}\n")
+    
     inicio = time.time()
     MergeSort(lista_alt)
     fim = time.time()
@@ -70,16 +70,16 @@ while True:
     print()
     total = (fim - inicio)
     print(f"Tempo levado com o MergeSort {total}s")
-    acessarMenu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
+    confirma_volta_menu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
 
-    if (acessarMenu == 1):
+    if (confirma_volta_menu == 1):
       pass
     else:
       quit()
   
-  elif (interacao == 5):
+  elif (opcao == 5):
     lista_alt = lista.copy()
-    print(f"\n{lista}\n")
+    
     inicio = time.time()
     QuickSort(lista_alt, 0, len(lista_alt) - 1)
     fim = time.time()
@@ -88,9 +88,9 @@ while True:
     print()
     total = (fim - inicio)
     print(f"Tempo levado com o QuickSort {total}s")
-    acessarMenu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
+    confirma_volta_menu = int(input('\nDeseja voltar ao menu (1-SIM ou 2-NÃO): '))
 
-    if (acessarMenu == 1):
+    if (confirma_volta_menu == 1):
       pass
     else:
       quit()
